bba8942f3041d6db22d6ac2e0e6d94d5c68d50da
leandro <leandro.estrada@itsgroup.com.br>
2020-05-31 13:00:02 -0300

    Adding the ArticleViewModel

---
+++ NewsApp/View Models/ArticleViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ArticleViewModel.swift
+//  NewsApp
+//
+//  Created by leandro de araujo estrada on 31/05/20.
+//  Copyright © 2020 Leandro Estrada. All rights reserved.
+//
+
+import Foundation
